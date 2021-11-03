355c7cbcf86b88b0e00a59b824ed0379ad1a5da9
sam <swzwij@gmail.com>
2021-11-03 10:06:26 +0100

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Components")]
+    private Rigidbody2D _rb;
+
+    [Header("Layer Masks")]
+    [SerializeField] private LayerMask _groundLayer;
+
+    [Header("Movement Variables")]
+    [SerializeField] private float _movementAcceleration;
+    [SerializeField] private float _maxMoveSpeed;
+    [SerializeField] private float _groundLinearDrag;
+    public bool _facingRight = true;
+    private float _horizontalDirection;
+    private bool _changingDirection => (_rb.velocity.x > 0f && _horizontalDirection < 0f) || (_rb.velocity.x < 0f && _horizontalDirection > 0f);
+
+    [Header("Jump Variables")]
+    [SerializeField] private float _jumpForce = 12f;
+    [SerializeField] private float _airLinearDrag = 2.5f;
+    [SerializeField] private float _fallMultiplier = 8f;
+    [SerializeField] private float _lowJumpFallMultiplier = 5f;
+    private bool _canJump => Input.GetButtonDown("Jump") && _onGround;
+
+    [Header("Ground Collision Variables")]
+    [SerializeField] private float _groundRaycastLength;
+    [SerializeField] private Vector3 _groundRaycastOffset;
+    public bool _onGround;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _horizontalDirection = GetInput().x;
+        if (_canJump)
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && _facingRight == true)
+        {
+            Flip();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && _facingRight == false)
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CheckCollision();
+        MoveCharacter();
+
+        if (_onGround)
+        {
+            ApplyGroundLinearDrag();
+        }
+        else
+        {
+            ApplyAirLinearDrag();
+            FallMultiplier();
+        }
+
+    }
+
+    private Vector2 GetInput()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void MoveCharacter()
+    {
+        _rb.AddForce(new Vector2(_horizontalDirection, 0f) * _movementAcceleration);
+
+        if (Mathf.Abs(_rb.velocity.x) > _maxMoveSpeed)
+        {
+            _rb.velocity = new Vector2(Mathf.Sign(_rb.velocity.x) * _maxMoveSpeed, _rb.velocity.y);
+        }
+    }
+
+    private void ApplyGroundLinearDrag()
+    {
+        if (Mathf.Abs(_horizontalDirection) < 0.4f || _changingDirection)
+        {
+            _rb.drag = _groundLinearDrag;
+        }
+        else
+        {
+            _rb.drag = 0f;
+        }
+    }
+
+    private void ApplyAirLinearDrag()
+    {
+        _rb.drag = _airLinearDrag;
+    }
+
+    private void Jump()
+    {
+        _rb.velocity = new Vector2(_rb.velocity.x, 0f);
+        _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void FallMultiplier()
+    {
+        if (_rb.velocity.y < 0)
+        {
+            _rb.gravityScale = _fallMultiplier;
+        }
+        else if (_rb.velocity.y > 0 && !Input.GetButtonDown("Jump"))
+        {
+            _rb.gravityScale = _lowJumpFallMultiplier;
+        }
+        else
+        {
+            _rb.gravityScale = 1f;
+        }
+    }
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+
+        transform.Rotate(Vector3.up * 180);
+    }
+
+    private void CheckCollision()
+    {
+        _onGround = Physics2D.Raycast(transform.position + _groundRaycastOffset, Vector2.down, _groundRaycastLength, _groundLayer) ||
+                    Physics2D.Raycast(transform.position - _groundRaycastOffset, Vector2.down, _groundRaycastLength, _groundLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * _groundRaycastLength);
+    }
+}
